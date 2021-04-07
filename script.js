3c12f32af55f4879bd73f210cd4502302cbed994
Alberto Roldán <a-f-r@hotmail.es>
2021-04-07 18:24:28 +0200

    Add some responsiveness

--- script.js
+++ script.js
@@ -65,12 +65,19 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// // for (const item of movements) {
+// //   item > 0 && console.log(`Deposit of ${item}`);
+// //   item < 0 && console.log(`Withdrawal of ${Math.abs(item)}`);
+// // }
 
-/////////////////////////////////////////////////
+// movements.forEach(function (movement, index, array) {
+//   console.log(index + 1, movement);
+// });
+
+// const currencies = new Set([['USD', 'EUR', 'GBP', 'GBP']]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(key, value);
+// });
